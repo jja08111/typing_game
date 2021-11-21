38c1f9d404e41b2957d2c804f5d44c9ecec53608
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-11-21 16:59:05 +0900

    Create UserCharactorPanel.java

--- src/view/GameGroundPanel.java
+++ src/view/GameGroundPanel.java
@@ -3,10 +3,23 @@ package view;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * {@link UserCharactorPanel}과 {@link EnemyHandler}를 통해 
+ * 생성된 {@link EnemyPanel}들이 있다.  
+ */
 public class GameGroundPanel extends JPanel {
+
+	private final UserCharactorPanel userPanel = new UserCharactorPanel();
 	
 	public GameGroundPanel() {
 		setLayout(null);
+		
+		add(userPanel);
+		userPanel.setLocation(0, 180);
+	}
+	
+	public UserCharactorPanel getUserPanel() {
+		return userPanel;
 	}
 	
 }
---
+++ src/view/UserCharactorPanel.java
@@ -0,0 +1,18 @@
+package view;
+
+import constant.Icons;
+
+public class UserCharactorPanel extends CharactorPanel {
+
+	public UserCharactorPanel() {
+		super(Icons.userCharactor);
+		setSize(100, 100);
+	}
+	
+	/**
+	 * @return 캐릭터의 오른쪽 끝의 위치를 반환한다.
+	 */
+	public int getRightX() {
+		return getX() + getWidth();
+	}
+}
