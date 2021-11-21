1aa869664bde46b5c12f61bd1352e56895955238
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-11-21 16:59:58 +0900

    Add userCharactor icon in Icons.java

--- src/constant/Icons.java
+++ src/constant/Icons.java
@@ -2,9 +2,13 @@ package constant;
 import javax.swing.ImageIcon;
 
 public final class Icons {
+	
 	public static final ImageIcon normal = new ImageIcon("normal.png");
 	
 	public static final ImageIcon pressed = new ImageIcon("pressed.png");
 	
 	public static final ImageIcon over = new ImageIcon("over.png");
+	
+	public static final ImageIcon userCharactor = new ImageIcon("over.png");
+	
 }
