c917faceeaa505d16cbca3079af98ca2acc9d6b5
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-11-29 00:03:09 +0900

    Remove useless importing

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,27 +1,15 @@
 package view;
 
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JSplitPane;
-import javax.swing.JTextField;
 
 import constant.TextStyle;
-import handler.EnemyHandler;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.FlowLayout;
 
 public class MainFrame extends JFrame {
